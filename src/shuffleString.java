6ca1926ffe2e5d0b39e47d1e9a10c5696c0af7d4
user2695 <zy.khan37@gmail.com>
2022-03-25 23:11:44 +0530

    1528. Shuffle String

--- src/shuffleString.java
+++ src/shuffleString.java
@@ -1,7 +1,7 @@
 public class shuffleString {
     public static void main(String[] args) {
-        int[] indices = { 4, 5, 6, 7,0, 2, 1, 3 };
-        String s = "codeleet!";
+        int[] indices = { 4, 5, 6, 7, 0, 2, 1, 3 };
+        String s = "codeleet";
         char res[] = new char[s.length()];
         for (int i = 0; i < indices.length; i++) {
             res[indices[i]] = s.charAt(i);
