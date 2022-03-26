3197e0c7b50caaa804113f34fa9dbde1421cb823
user2695 <zy.khan37@gmail.com>
2022-03-26 21:34:14 +0530

    1528. Shuffle String

--- src/shuffleString.java
+++ src/shuffleString.java
@@ -2,12 +2,14 @@ public class shuffleString {
     public static void main(String[] args) {
         int[] indices = { 4, 5, 6, 7, 0, 2, 1, 3 };
         String s = "codeleet";
+        System.out.println(shuffleStr(indices, s));
+    }
+
+    static char[] shuffleStr(int[] indices, String s) {
         char res[] = new char[s.length()];
         for (int i = 0; i < indices.length; i++) {
             res[indices[i]] = s.charAt(i);
         }
-        for (int j = 0; j < res.length; j++) {
-            System.out.print(res[j]);
-        }
+        return res;
     }
 }
