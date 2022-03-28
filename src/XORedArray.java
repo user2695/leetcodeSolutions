8d3408d9925cfbdb87c4e77801e78ddc8025b18d
user2695 <zy.khan37@gmail.com>
2022-03-28 12:49:19 +0530

    1720. Decode XORed Array

--- src/XORedArray.java
+++ src/XORedArray.java
@@ -1,18 +1,19 @@
 public class XORedArray {
     public static void main(String[] args) {
-        int[] encoded = { 6, 2, 7, 3 };
-        int first = 4;
-        decodeArray(encoded, first);
+        int[] encoded = { 1, 2, 3 };
+        int first = 1;
+        int[] ans = new int[encoded.length + 1];
+        ans = decodeArray(encoded, first);
+        for (int i = 0; i < ans.length; i++)
+            System.out.println(ans[i]);
     }
 
-    static void decodeArray(int[] encoded, int first) {
+    static int[] decodeArray(int[] encoded, int first) {
         int[] arr = new int[encoded.length + 1];
         arr[0] = first;
         for (int i = 1; i < arr.length; i++) {
             arr[i] = arr[i - 1] ^ encoded[i - 1];
         }
-        for (int i=0;i<arr.length;i++)
-            System.out.println(arr[i]);
-
+        return arr;
     }
 }
