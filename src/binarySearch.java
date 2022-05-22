5b61af9f8b688a0e133ada51189ab6bb394cf89b
user2695 <zy.khan37@gmail.com>
2022-05-22 12:30:24 +0530

    704. Binary Search

---
+++ src/binarySearch.java
@@ -0,0 +1,23 @@
+public class binarySearch {
+    public static void main(String[] args) {
+        int arr[] = { -1, 0, 3, 5, 9, 12 };
+        int target = 9;
+        System.out.println(search(arr, target));
+    }
+
+    static int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        int mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < mid) {
+                r = mid - 1;
+            } else if (target > mid) {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
