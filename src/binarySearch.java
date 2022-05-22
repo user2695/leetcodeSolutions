4b86429637601e835cd0cd6a233638492a24d7ac
user2695 <zy.khan37@gmail.com>
2022-05-22 13:17:17 +0530

    704. Binary Search

--- src/binarySearch.java
+++ src/binarySearch.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class binarySearch {
     public static void main(String[] args) {
         int arr[] = { -1, 0, 3, 5, 9, 12 };
@@ -5,19 +7,24 @@ public class binarySearch {
         System.out.println(search(arr, target));
     }
 
+    // static int search(int[] nums, int target) {
+    // int l = 0, r = nums.length - 1;
+    // int mid;
+    // while (l <= r) {
+    // mid = l + (r - l) / 2;
+    // if (nums[mid] == target) {
+    // return mid;
+    // } else if (target < mid) {
+    // r = mid - 1;
+    // } else if (target > mid) {
+    // l = mid + 1;
+    // }
+    // }
+    // return -1;
+    // }
     static int search(int[] nums, int target) {
-        int l = 0, r = nums.length - 1;
-        int mid;
-        while (l <= r) {
-            mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (target < mid) {
-                r = mid - 1;
-            } else if (target > mid) {
-                l = mid + 1;
-            }
-        }
-        return -1;
+        int index = Arrays.binarySearch(nums, target);
+        return (index < 0) ? -1 : index;
     }
+
 }
