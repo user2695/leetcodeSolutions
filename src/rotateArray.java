f337db84f3c9cae516058c7498063da2258e3dbf
user2695 <zy.khan37@gmail.com>
2022-05-23 11:47:31 +0530

    189. Rotate Array

--- src/rotateArray.java
+++ src/rotateArray.java
@@ -1,23 +1,27 @@
 public class rotateArray {
     public static void main(String[] args) {
 
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-        leftRotate(arr, 2, 7);
+        int arr[] = { 1,2,3,4,5,6,7 };
+        int k = 3;
+        rotate(arr, k);
         for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i]);
+            System.out.print(arr[i] + " ");
     }
 
-    static void leftRotate(int arr[], int d, int n) {
-        for (int i = 0; i < d; i++) {
-            leftRotatebyOne(arr, n);
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        while (start <= end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-    }
-
-    static void leftRotatebyOne(int arr[], int n) {
-        int temp, i;
-        temp = arr[0];
-        for (i = 0; i < n - 1; i++)
-            arr[i] = arr[i + 1];
-        arr[n - 1] = temp;
     }
 }
