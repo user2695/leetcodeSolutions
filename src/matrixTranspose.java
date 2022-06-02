7518e4af81281053067699d3221aa218b292324c
user2695 <zy.khan37@gmail.com>
2022-06-02 10:53:14 +0530

    867. Transpose Matrix

---
+++ src/matrixTranspose.java
@@ -0,0 +1,23 @@
+public class matrixTranspose {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] ans = transpose(matrix);
+        for (int i = 0; i < ans.length; i++) {
+            for (int j = 0; j < ans[i].length; j++) {
+                System.out.print(ans[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    static int[][] transpose(int[][] matrix) {
+        int[][] ans = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+}
