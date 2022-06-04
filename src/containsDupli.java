10a2727d1964e0e2668dae2bcb569ccc1d904999
user2695 <zy.khan37@gmail.com>
2022-06-04 20:15:17 +0530

    217. Contains Duplicate

---
+++ src/containsDupli.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+public class containsDupli {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        System.out.println(containsDuplicate(nums));
+    }
+
+    static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i]))
+                return true;
+        }
+        return false;
+    }
+}
