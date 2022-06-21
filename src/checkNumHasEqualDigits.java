7e15d734165b0657036a51f651913fbaf33280cb
user2695 <zy.khan37@gmail.com>
2022-06-21 19:46:58 +0530

    2283. Check if Number Has Equal Digit Count and Digit Value

---
+++ src/checkNumHasEqualDigits.java
@@ -0,0 +1,22 @@
+public class checkNumHasEqualDigits {
+    public static void main(String[] args) {
+        String num = "1210";
+        System.out.println(digitCount(num));
+    }
+
+    static boolean digitCount(String num) {
+        int indexCount[] = new int[10];
+
+        for (char c : num.toCharArray()) {
+            indexCount[c - '0']++;
+        }
+
+        for (int i = 0; i < num.length(); i++) {
+            if (Character.getNumericValue(num.charAt(i)) != indexCount[i])
+                return false;
+        }
+
+        return true;
+
+    }
+}
