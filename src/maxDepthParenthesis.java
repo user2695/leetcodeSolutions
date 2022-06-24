e2a055b8ba2288fa752913fc72d35829bd834855
user2695 <zy.khan37@gmail.com>
2022-06-24 17:56:42 +0530

    1614. Maximum Nesting Depth of the Parentheses

---
+++ src/maxDepthParenthesis.java
@@ -0,0 +1,20 @@
+public class maxDepthParenthesis {
+    public static void main(String[] args) {
+        String s = "(1+(2*3)+((8)/4))+1";
+        System.out.println(maxDepth(s));
+    }
+
+    static int maxDepth(String s) {
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                count++;
+            } else if (s.charAt(i) == ')') {
+                count--;
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+}
