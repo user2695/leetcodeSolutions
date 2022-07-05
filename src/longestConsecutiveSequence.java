fb92f60774be972fb6373959f478e140ab00daa0
user2695 <zy.khan37@gmail.com>
2022-07-05 12:23:46 +0530

    128. Longest Consecutive Sequence

---
+++ src/longestConsecutiveSequence.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class longestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] nums = { 100, 4, 200, 1, 3, 2 };
+        System.out.println(longestConsecutive(nums));
+    }
+
+    static int longestConsecutive(int[] nums) {
+        int max = 0;
+
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int count = 1;
+
+            // look left
+            int num = nums[i];
+            while (set.contains(--num)) {
+                count++;
+                set.remove(num);
+            }
+
+            // look right
+            num = nums[i];
+            while (set.contains(++num)) {
+                count++;
+                set.remove(num);
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+}
