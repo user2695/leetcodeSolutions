e8e9efb1314729abcfcac0643f36c76d3a391eb5
user2695 <zy.khan37@gmail.com>
2022-07-07 22:04:08 +0530

    97. Interleaving String

---
+++ src/interleavingStrings.java
@@ -0,0 +1,31 @@
+public class interleavingStrings {
+    public static void main(String[] args) {
+        String s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac";
+        System.out.println(isInterleave(s1, s2, s3));
+    }
+
+    private static boolean dfs(String s1, String s2, String s3, int i, int j, int k, boolean[][] visited) {
+        if (i == s1.length() && j == s2.length())
+            return true;
+        if (i > s1.length() || j > s2.length())
+            return false;
+        if (visited[i][j])
+            return false;
+        visited[i][j] = true;
+        boolean a = false, b = false;
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            a = dfs(s1, s2, s3, i + 1, j, k + 1, visited);
+        }
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            b = dfs(s1, s2, s3, i, j + 1, k + 1, visited);
+        }
+        return a || b;
+    }
+
+    public static boolean isInterleave(String s1, String s2, String s3) {
+        if (s3.length() < (s1.length() + s2.length()) || s3.length() > (s1.length() + s2.length()))
+            return false;
+        boolean[][] visited = new boolean[s1.length() + 1][s2.length() + 1];
+        return dfs(s1, s2, s3, 0, 0, 0, visited);
+    }
+}
