29693e1697a793a14e8a99d58bece7d8445fa8e8
user2695 <zy.khan37@gmail.com>
2022-07-12 10:43:00 +0530

    473. Matchsticks to Square

---
+++ src/matchsticksToSquare.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class matchsticksToSquare {
+    public static void main(String[] args) {
+        int[] M = { 1, 1, 2, 2, 2 };
+        System.out.println(makesquare(M));
+    }
+
+    public static boolean makesquare(int[] M) {
+
+        Arrays.sort(M);
+        int total = 0;
+        for (int i = 0; i < M.length; i++)
+            total += M[i];
+        side = total / 4;
+        if ((float) total / 4 > side || M[M.length - 1] > side)
+            return false;
+        return btrack(M.length - 1, side, 0, M);
+    }
+
+    public static int side;
+
+    public static boolean btrack(int i, int space, int done, int[] M) {
+        if (done == 3)
+            return true;
+        for (; i >= 0; i--) {
+            int num = M[i];
+            boolean res;
+            if (num > space)
+                continue;
+            M[i] = side + 1;
+            if (num == space)
+                res = btrack(M.length - 2, side, done + 1, M);
+            else
+                res = btrack(i - 1, space - num, done, M);
+            if (res)
+                return true;
+            M[i] = num;
+            while (i > 0 && M[i - 1] == num)
+                i--;
+        }
+        return false;
+    }
+}
