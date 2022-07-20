1b764d5176ded7a731fc5c871699c74b89d5fdd8
user2695 <zy.khan37@gmail.com>
2022-07-20 11:53:01 +0530

    792. Number of Matching Subsequences

---
+++ src/numberOfMatchingSubsequences.java
@@ -0,0 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class numberOfMatchingSubsequences {
+    public static void main(String[] args) {
+        String s = "dsahjpjauf";
+        String[] words = { "ahjpjau", "ja", "ahbwzgqnuk", "tnmlanowax" };
+        System.out.println(numMatchingSubseq(s, words));
+    }
+
+    static int numMatchingSubseq(String s, String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String str : words) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+
+        int ans = 0;
+        char ch[] = s.toCharArray();
+
+        for (String str : map.keySet()) {
+
+            char temp[] = str.toCharArray();
+            int i = 0;
+            int j = 0;
+
+            while (i < ch.length && j < temp.length) {
+                if (ch[i] == temp[j]) {
+                    i++;
+                    j++;
+                } else {
+                    i++;
+                }
+            }
+
+            if (j == temp.length) {
+                ans += map.get(str);
+            }
+
+        }
+
+        return ans;
+    }
+}
