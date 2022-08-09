1d07503881ba9d393419d4eb7624e23b8384bb8a
user2695 <zy.khan37@gmail.com>
2022-08-09 12:33:48 +0530

    49. Group Anagrams

---
+++ src/groupAnagrams.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class groupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        System.out.println(groupAnagram(strs));
+    }
+
+    static List<List<String>> groupAnagram(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = new char[26];
+            for (char c : s.toCharArray())
+                ca[c - 'a']++;
+            String keyStr = String.valueOf(ca);
+            if (!map.containsKey(keyStr))
+                map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
